124985bb2ecf518c19b0a85d821e100e2c7b7dfd
zzzzeu <zouanei@outlook.com>
2020-07-17 20:16:38 +0800

    42.trapping rain water

--- List/42.Trapping rain water.py
+++ List/42.Trapping rain water.py
@@ -15,3 +15,22 @@ class Solution:
                 res += maxright - height[right]
                 right -= 1
         return res
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height: 
+            return 0
+        n = len(height)
+        maxleft = [0] * n
+        maxright = [0] * n
+        res = 0
+        maxleft[0] = height[0]
+        maxright[n-1] = height[n-1]
+        for i in range(1,n):
+            maxleft[i] = max(height[i],maxleft[i-1])
+        for j in range(n-2,-1,-1):
+            maxright[j] = max(height[j],maxright[j+1])
+        for i in range(n):
+            if min(maxleft[i],maxright[i]) > height[i]:
+                res += min(maxleft[i],maxright[i]) - height[i]
+        return res
