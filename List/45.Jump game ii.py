9b04cdcdeeebf8219fe9c138c94b83e499d8f5ff
zzzzeu <zouanei@outlook.com>
2020-07-31 17:57:00 +0800

    45.jump game ii

---
+++ List/45.Jump game ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxPos, end, step = 0, 0, 0
+        for i in range(n - 1):
+            if maxPos >= i:
+                maxPos = max(maxPos, i + nums[i])
+                if i == end:
+                    end = maxPos
+                    step += 1
+        return step
