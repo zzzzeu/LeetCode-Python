3444276a6a8c11dc184a7748100cb1fda63c740d
zzzzeu <zouanei@outlook.com>
2020-08-16 22:06:11 +0800

    69.sqrtx

---
+++ List/69.Sqrtx.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l, r, ans = 0, x, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
