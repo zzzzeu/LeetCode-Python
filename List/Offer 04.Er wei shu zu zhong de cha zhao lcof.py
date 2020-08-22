7f6a2c9a27d696898515eb26660d0dd8e7a9a92a
zzzzeu <zouanei@outlook.com>
2020-08-22 22:35:08 +0800

    Offer 04.er wei shu zu zhong de cha zhao lcof

---
+++ List/Offer 04.Er wei shu zu zhong de cha zhao lcof.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findNumberIn2DArray(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target: i -= 1
+            elif matrix[i][j] < target: j += 1
+            else: return True
+        return False
