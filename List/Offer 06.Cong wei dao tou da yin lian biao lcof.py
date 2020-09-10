0a9b6b9737013b7c977baa8eec3cf50b4ec8219e
zzzzeu <zouanei@outlook.com>
2020-09-10 20:02:18 +0800

    Offer 06.cong wei dao tou da yin lian biao lcof

---
+++ List/Offer 06.Cong wei dao tou da yin lian biao lcof.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reversePrint(self, head):
+        """
+        :type head: ListNode
+        :rtype: List[int]
+        """
+        return self.reversePrint(head.next) + [head.val] if head else []
