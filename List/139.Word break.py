7ec77a0c016601c460182f1b3d46be5992f64198
zzzzeu <zouanei@outlook.com>
2020-10-02 23:01:55 +0800

    139.word break

---
+++ List/139.Word break.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                if dp[i] and (s[i:j] in wordDict):
+                    dp[j] = True
+        return dp[-1]
+        
